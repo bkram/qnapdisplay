f8bde72d571f106245ba18786eb55a5a08bc0938
Mark de Bruijn <mrdebruijn@gmail.com>
2022-01-10 15:50:27 +0100

    Make pylint happy about setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+"""
+Qnapdisplay, an lcd python module for Qnap.
+Features both writing to the display as wel as reading key presses from the front panel.
+It was developed on a Qnap TS-459, but it will work on some other models as well.
+"""
 from setuptools import setup
 
 setup(name='qnapdisplay',
-      version='0.1',
+      version='0.2',
       description='Library for interacting with front-panel displays on Qnap products',
       url='http://github.com/bkram/qnapdisplay',
       author='Mark de Bruijn',
